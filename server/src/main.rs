662472752bdbb844d6f4c4cb2855b247aa49a6d3
Dave <david.tomusk@gmail.com>
2026-06-01 18:06:09 +0100

    Create basic server with one get endpoint

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+
+// tokio multithreaded runtime needs to be enabled, use full features for simplicity
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(handler));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .unwrap();
+
+    println!("Server running on http://localhost:3000");
+
+    axum::serve(listener, app)
+        .await
+        .unwrap();
+}
+
+async fn handler() -> &'static str {
+    "Hello, World!"
 }
