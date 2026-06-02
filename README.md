ec70e9387c6cdcc7bc8af0ec9faf85f74905ea09
Dave <david.tomusk@gmail.com>
2026-06-02 16:16:34 +0100

    Add port env variable, env variable file, and update docs

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # template-server-rust
 A template for a RESTful API server written in Rust that can be forked for other projects
+
+## Technology 
+The server is written in Rust using Axum.
+
+## Architecture
+-
+
+## Deployment 
+The server can be run locally via Docker. `Dockerfile` contains instructions to build the image and run the server container, and `docker-compose.yml` can be used to run the container.
+
+## Environment variables 
+Env variables are stored locally in `.env`. `.env.example` is a copy of `.env` that is stored in source control and gives example values of env variables.
--- server/src/main.rs
+++ server/src/main.rs
@@ -3,17 +3,21 @@ use axum::{
     Router,
 };
 
+use std::env;
+
 // tokio multithreaded runtime needs to be enabled, use full features for simplicity
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(handler));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let addr = format!("0.0.0.0:{}", port);
+    let listener = tokio::net::TcpListener::bind(&addr)
         .await
         .unwrap();
 
-    println!("Server running on http://localhost:3000");
+    println!("Server running on http://{}", addr);
 
     axum::serve(listener, app)
         .await
